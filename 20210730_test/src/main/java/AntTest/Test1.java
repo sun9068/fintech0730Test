5620c22ea48dbfbe7b8882d533889054708fa76b
KTE <KTE@DESKTOP-CTAQ8L3>
2021-09-29 12:29:27 +0900

    0730Test

---
+++ 20210730_test/src/main/java/AntTest/Test1.java
@@ -0,0 +1,10 @@
+package AntTest;
+
+public class Test1 {
+	public static void main(String[] args) {
+
+		System.out.println("이 프로그램은 Test1.java 입니다.");
+
+	}
+
+}
---
+++ 20210730_test/src/main/java/AntTest/Test2.java
@@ -0,0 +1,10 @@
+package AntTest;
+
+public class Test2 {
+	public static void main(String[] args) {
+
+		System.out.println("이 프로그램은 Test2.java 입니다.");
+
+	}
+
+}
